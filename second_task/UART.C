6af68386c84a8d080d058ae0ba44732df18aa695
Ahmed moh abd <ahmed.moh.abd.911@gmail.com>
2025-08-09 14:22:34 +0300

    the task of  UART communication modified

--- second_task/UART.C
+++ second_task/UART.C
@@ -1,14 +1,22 @@
 #define FOSC 16000000UL
 #define BAUD 9600
-#define MYUBRR FOSC/16/BAUD-1
+#define MYUBRR FOSC/16/BAUD-1 // this calculates the baud rate
 #include <avr/io.h>
 
 
 
 void USART_Init(unsigned int ubrr)
 {
-  
-  UBRR0H = (unsigned char)(ubrr>>8);
+  /* 
+
+  the UBRR is the baud rate setting and is splited into two parts the high bits and the lower one ,
+  the way  "" >> 8 takes only the high byte ,
+  the unsigned char make the register takes the low bytes only .
+
+  */
+
+
+  UBRR0H = (ubrr>>8); 
   UBRR0L = (unsigned char)ubrr;
   
   UCSR0B = (1<<RXEN0)|(1<<TXEN0);
@@ -18,7 +26,7 @@ void USART_Init(unsigned int ubrr)
 
 unsigned char USART_RECEIVE()
 {
-  while (!(UCSR0A & (1<<RXC0)));
+  while (!(UCSR0A & (1<<RXC0))); // wait til there is data available to read
   
   	return UDR0;
 }
@@ -26,7 +34,7 @@ unsigned char USART_RECEIVE()
 void USART_TRANSMIT(unsigned char DATA)
 {
 
-  while (!(UCSR0A & (1<<UDRE0)));
+  while (!(UCSR0A & (1<<UDRE0))); // wait till the  buffer is empty and ready to send new data 
     
   UDR0 = DATA + 1 ;
   

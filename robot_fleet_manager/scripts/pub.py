75d076bc964c70f184d3de5cb6dd1a4cd64c48d6
Ahmed Mohamed Abdallah <ahmed.moh.abd.911@gmail.com>
2025-09-14 16:22:57 +0300

    this is my first ros task !!!

---
+++ robot_fleet_manager/scripts/pub.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import rospy
+from robot_fleet_manager.msg import custom
+
+
+def talker():
+
+  msg = custom()
+  msg.name ='Ahmed_robot'
+  msg.battery = 54
+  pub.publish(msg)
+
+
+
+
+if __name__ == '__main__' :
+    
+      
+      rospy.init_node('talker') 
+      rospy.loginfo("message is sent !!!")
+      
+      pub = rospy.Publisher("custom_message" ,custom ,queue_size = 10)
+      rate = rospy.Rate(10)
+      
+      
+      while not rospy.is_shutdown():
+
+        talker()
+        rate.sleep()
+
+      
---
+++ robot_fleet_manager/scripts/sub.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+import rospy
+from robot_fleet_manager.msg import custom
+from pub import talker
+
+
+
+if __name__ == '__main__' :
+
+    rospy.init_node("listener")
+    
+    rospy.Subscriber("received_message" , custom , callback= talker)
+
+    rospy.loginfo("message is received  !!!")
+
+    rospy.spin()
